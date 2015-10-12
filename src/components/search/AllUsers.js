da22d5ffa5a94268af1275cd743335c423080995
NickTroutwine <Nat88120@ucmo.edu>
2015-10-12 17:07:17 -0500

    project not rebuilding with updates

--- src/components/search/AllUsers.js
+++ src/components/search/AllUsers.js
@@ -9,23 +9,24 @@ getInitialState: function() {
 },
 onChangeOption: function(){
 	window.location.pathname = '/project/'+ this.props.route;
-}
+},
 componentDidMount: function() {
 	$.getJSON('/api/users', function(result) {
     result = result.map(function (element, index) {
-      return ({name: element.username}<br/>)
+      return ({value: element.username, label: element.username})
     })
     this.setState({options: result});
     }.bind(this))
   },
-},
 	render: function() {
 		return (
 			<div className="row">
+			<form>
 				<div className="form-group col-xs-8 col-xs-offset-2 col-sm-8 text-center">
 					<label>All Users</label>
 					<Select name="form-field-name" placeholder={this.state.options} required pattern="^[a-zA-Z][a-zA-Z0-9\.]{1,50}$" options={this.state.options}  onChange={this.onChangeOption} className="text-left" />
 				</div>
+			</form>
 			</div>
 		);
 	}
